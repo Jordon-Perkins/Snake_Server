19264e3586c8c947ef8e5287317eed7c5a8326c2
Jordon Perkins <jordon.rubel@gmail.com>
2023-01-26 09:32:59 -0600

    single species ticket #3

--- views/specie_requests.py
+++ views/specie_requests.py
@@ -23,11 +23,27 @@ def get_all_species():
     return species
 
 def get_single_specie(id):
-    requested_specie = None
-    for specie in Specie:
-        if specie["id"] == id:
-            requested_specie = specie
-    return requested_specie
+    with sqlite3.connect("./snake.sqlite3") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        # Use a ? parameter to inject a variable's value
+        # into the SQL statement.
+        db_cursor.execute("""
+        SELECT
+            a.id,
+            a.name
+        FROM Species a
+        WHERE a.id = ?
+        """, ( id, ))
+
+        # Load the single result into memory
+        data = db_cursor.fetchone()
+
+        # Create an animal instance from the current row
+        specie = Specie(data['id'], data['name'])
+
+        return specie.__dict__
 
 
 def create_specie(specie):
